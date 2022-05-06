4f02c190fc666ba7cf0181f1266493f32ae4efd6
Diego Priess <diegopriess.dev@gmail>
2022-05-05 22:34:00 -0300

    Fazendo teleporte de fase para escritorio

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Portal : MonoBehaviour
-{
-    void OnTriggerEnter(Collider other) 
-    {
-        print("Ok!");
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(AudioSource))]
+
+public class Portal : MonoBehaviour {
+ 
+    public string sceneToTeleport;
+
+    void OnTriggerEnter (Collider other) {
+        SceneManager.LoadScene(sceneToTeleport);
     }
 }
